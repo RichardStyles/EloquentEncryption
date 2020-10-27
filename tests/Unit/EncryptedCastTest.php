564bc4a32c04e32342243a27e58ba92cd58db165
Richard Styles <richardstyles@gmail.com>
2020-10-27 20:13:40 +0000

    Update cast test to remove unused code

--- tests/Unit/EncryptedCastTest.php
+++ tests/Unit/EncryptedCastTest.php
@@ -5,35 +5,22 @@ namespace RichardStyles\EloquentEncryption\Tests\Unit;
 
 
 use Illuminate\Foundation\Auth\User;
-use Illuminate\Support\Facades\Storage;
 use RichardStyles\EloquentEncryption\Casts\Encrypted;
 use RichardStyles\EloquentEncryption\EloquentEncryptionFacade;
 use RichardStyles\EloquentEncryption\Tests\TestCase;
-use RichardStyles\EloquentEncryption\Tests\Traits\WithRSAHelpers;
 
 class EncryptedCastTest extends TestCase
 {
-    use WithRSAHelpers;
-
-    public function setUp(): void
-    {
-        parent::setUp();
-
-        Storage::fake();
-    }
-
     /** @test */
     function encrypted_cast_decrypts_values()
     {
-        EloquentEncryptionFacade::partialMock()
-            ->shouldReceive('exists')
+        EloquentEncryptionFacade::shouldReceive('exists')
             ->andReturn(true)
             ->shouldReceive('decrypt')
             ->with('001100110011')
             ->andReturn('test');
 
         $cast = new Encrypted();
-
         $user = new User();
 
         $this->assertEquals('test', $cast->get($user, 'encrypted', '001100110011', []));
@@ -42,15 +29,13 @@ class EncryptedCastTest extends TestCase
     /** @test */
     function encrypted_cast_encrypts_values()
     {
-        EloquentEncryptionFacade::partialMock()
-            ->shouldReceive('exists')
+        EloquentEncryptionFacade::shouldReceive('exists')
             ->andReturn(true)
             ->shouldReceive('encrypt')
             ->with('test')
             ->andReturn('001100110011');
 
         $cast = new Encrypted();
-
         $user = new User();
 
         $this->assertEquals('001100110011', $cast->set($user, 'encrypted', 'test', []));
