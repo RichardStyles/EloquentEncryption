17bcc4f4b1283e0fd46514aa12b57a560664d64b
Richard Styles <richardstyles@gmail.com>
2020-10-27 20:14:51 +0000

    Add EncryptedCollection cast

---
+++ src/Casts/EncryptedCollection.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace RichardStyles\EloquentEncryption\Casts;
+
+
+use Illuminate\Database\Eloquent\JsonEncodingException;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Collection as BaseCollection;
+
+class EncryptedCollection extends Encrypted
+{
+    /**
+     * Cast the given value and decrypt
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  array  $attributes
+     * @return int
+     */
+    public function get($model, $key, $value, $attributes)
+    {
+        return new BaseCollection(json_decode(parent::get($model, $key, $value, $attributes)));
+    }
+
+    /**
+     * Prepare the given value for storage.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  array  $value
+     * @param  array  $attributes
+     * @return string
+     */
+    public function set($model, $key, $value, $attributes)
+    {
+        if($value instanceof Collection){
+            $value = $value->toJson();
+        }else {
+            $value = json_encode($value);
+
+            if ($value === false) {
+                throw JsonEncodingException::forAttribute(
+                    $this, $key, json_last_error_msg()
+                );
+            }
+        }
+        dd($value);
+        return parent::set($model, $key, $value, $attributes);
+    }
+}
---
+++ tests/Unit/EncryptedCollectionCastTest.php
@@ -0,0 +1,65 @@
+<?php
+
+
+namespace RichardStyles\EloquentEncryption\Tests\Unit;
+
+
+use Illuminate\Database\Eloquent\JsonEncodingException;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Collection;
+use RichardStyles\EloquentEncryption\Casts\EncryptedCollection;
+use RichardStyles\EloquentEncryption\EloquentEncryptionFacade;
+use RichardStyles\EloquentEncryption\Tests\TestCase;
+
+class EncryptedCollectionCastTest extends TestCase
+{
+    /** @test */
+    function encrypted_collection_cast_decrypts_values()
+    {
+        EloquentEncryptionFacade::shouldReceive('exists')
+            ->andReturn(true)
+            ->shouldReceive('decrypt')
+            ->with('001100110011')
+            ->andReturn('{"test":"a","foo":"bar","bar":{"test":"result"}}');
+
+        $cast = new EncryptedCollection();
+        $user = new User();
+
+        $response = $cast->get($user, 'encrypted', '001100110011', []);
+
+        $this->assertInstanceOf(Collection::class, $response);
+    }
+
+    /** @test */
+    function encrypted_collection_cast_encrypts_values()
+    {
+        $collect = collect([
+            'test' => 'a',
+            'foo'  => 'bar',
+            'bar'  => [
+                'test' => 'result'
+            ]
+        ]);
+        EloquentEncryptionFacade::partialMock()
+            ->shouldReceive('exists')
+            ->andReturn(true)
+            ->shouldReceive('encrypt')
+            ->with('{"test":"a","foo":"bar","bar":{"test":"result"}}')
+            ->andReturn('001100110011');
+
+        $cast = new EncryptedCollection();
+        $user = new User();
+
+        $this->assertEquals('001100110011', $cast->set($user, 'encrypted', $collect, []));
+    }
+
+    /** @test */
+    function cannot_encrypted_invalid_array_values()
+    {
+        $cast = new EncryptedCollection();
+        $user = new User();
+
+        $this->expectException(JsonEncodingException::class);
+        $this->assertEquals('001100110011', $cast->set($user, 'encrypted', "\xB1\x31", []));
+    }
+}
